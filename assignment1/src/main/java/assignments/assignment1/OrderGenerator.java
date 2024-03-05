73baec64397adebd23214103739824c787118170
alphasuthamedia <alpha.sutha@ui.ac.id>
2024-03-05 10:14:03 +0700

    Fixing wrong select menu input

--- assignment1/src/main/java/assignments/assignment1/OrderGenerator.java
+++ assignment1/src/main/java/assignments/assignment1/OrderGenerator.java
@@ -190,6 +190,8 @@ public class OrderGenerator {
                         }
                     }
                 }
+            } else {
+                continue;
             }
         }
     }
