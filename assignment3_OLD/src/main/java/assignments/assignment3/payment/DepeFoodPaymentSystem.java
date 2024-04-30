387edec8ca33c8b5e69ac004cbaa30c22c2e15a9
alphasuthamedia <alpha.sutha@ui.ac.id>
2024-04-30 08:57:20 +0700

    fixing gradle - 3

--- assignment3_OLD/src/main/java/assignments/assignment3/payment/DepeFoodPaymentSystem.java
+++
@@ -1,9 +0,0 @@
-package assignments.assignment3.payment;
-
-public interface DepeFoodPaymentSystem {
-    public static final long saldo = 0;
-    public abstract long processPayment(long amount);
-    //TODO implementasikan interface di sini
-    // Atribut harap mengikuti dokumen soal 
-    // Anda dibebaskan untuk membuat method yang diperlukan
-}
